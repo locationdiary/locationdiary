4cee8fe8bfa7a642505f1a9af08da4044352fd02
Arthur Puyou <arthur@puyou.fr>
2019-05-20 17:38:50 +0800

    Remove profile routes and header

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,8 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 
-import Header from './header';
-
 // Code-splitting is automated for routes
 import Home from '../routes/home';
-import Profile from '../routes/profile';
 
 export default class App extends Component {
 	
@@ -20,11 +17,8 @@ export default class App extends Component {
 	render() {
 		return (
 			<div id="app">
-				<Header />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
 				</Router>
 			</div>
 		);
