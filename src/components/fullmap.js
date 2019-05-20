a23f859615967636683af1109461ea2d2938a31d
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-05-20 23:04:03 +0800

    handle map hot reload

--- src/components/fullmap.js
+++ src/components/fullmap.js
@@ -1,44 +1,81 @@
-import { h, Component } from 'preact';
-import 'leaflet/dist/leaflet.css';
-import pin from 'leaflet/dist/images/marker-icon.png';
-import pin2x from 'leaflet/dist/images/marker-icon-2x.png';
-import shadow from 'leaflet/dist/images/marker-shadow.png';
-import L from 'leaflet';
+import { h, Component } from "preact";
+import "leaflet/dist/leaflet.css";
+import pin from "leaflet/dist/images/marker-icon.png";
+import pin2x from "leaflet/dist/images/marker-icon-2x.png";
+import shadow from "leaflet/dist/images/marker-shadow.png";
+import L from "leaflet";
 
-import style from './fullmap.css';
+const customIcon = L.icon({
+  iconUrl: pin,
+  iconRetinaUrl: pin2x,
+  iconSize: [24, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: shadow,
+  shadowSize: [41, 41]
+});
+
+import style from "./fullmap.css";
 
 class FullMap extends Component {
+  addLocations(locations, map) {
+    const markers = this.state.markers;
+    locations.map(location => {
+      if (markers[location.id]) {
+        return null;
+      }
+      markers[location.id] = L.marker(
+        [location.coords.latitude, location.coords.longitude],
+        {
+          icon: customIcon
+        }
+      )
+        .addTo(map)
+        .bindPopup(location.message);
+    });
+    this.setState({
+      markers
+    });
+  }
   componentDidMount() {
     const position = [51.505, -0.09];
-    const map = L.map('map').setView(position, 13);
+    const map = L.map("map").setView(position, 2);
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: 'abcd',
-      maxZoom: 19
-    }).addTo(map);
+    L.tileLayer(
+      "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: "abcd",
+        maxZoom: 19
+      }
+    ).addTo(map);
 
-    const customIcon = L.icon({
-      iconUrl: pin,
-      iconRetinaUrl: pin2x,
-      iconSize: [24, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowUrl: shadow,
-      shadowSize: [41, 41],
+    if (this.props.locations) {
+      this.addLocations(this.props.locations, map);
+    }
+
+    this.setState({
+      map
     });
+  }
 
-    L.marker(position, {icon: customIcon})
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup. <br> Easily customizable.');
+  componentWillUnmount() {
+    this.state.map.remove();
+  }
+
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      markers: {}
+    };
+    this.addLocations = this.addLocations.bind(this);
   }
 
   render() {
-    return (
-      <div id="map" class={style.map}></div>
-    );
+    return <div id="map" class={style.map} />;
   }
-
 }
 
 export default FullMap;
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,10 +4,31 @@ import style from "./style";
 import FullMap from "../../components/fullmap";
 import Sidebar from "../../components/sidebar";
 
-const Home = ({session}) => (
+const TEST_LOCATIONS = [
+  {
+    coords: {
+      latitude: 12,
+      longitude: 12
+    },
+    message: "Hey test",
+    date: new Date(),
+    id: "b5e18746-a489-4f21-b318-1094ddacae4d"
+  },
+  {
+    coords: {
+      latitude: 12,
+      longitude: 12
+    },
+    message: "Hey test",
+    date: new Date(),
+    id: "b5e18746-a489-4f21-b318-1094ddacae4d"
+  }
+];
+
+const Home = ({ session }) => (
   <div class={style.home}>
     <div class={style.fullmap}>
-      <FullMap />
+      <FullMap locations={TEST_LOCATIONS} />
     </div>
     <div class={style.sidebar}>
       <Sidebar session={session} />
