4702ed7681fe71ed99a5e0208bab859837c7c485
Arthur Puyou <arthur@puyou.fr>
2019-05-20 19:14:27 +0800

    Add Sidebar

---
+++ src/components/sidebar.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+
+import style from './sidebar.css';
+
+const Sidebar = () => (
+	<div class={style.sidebar}>
+    Hey
+	</div>
+);
+
+export default Sidebar;
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,18 @@
 import { h } from 'preact';
 import style from './style';
+
 import FullMap from '../../components/fullmap';
+import Sidebar from '../../components/sidebar';
 
 const Home = () => (
-	<div class={style.home}>
-		<FullMap />
-	</div>
+  <div class={style.home}>
+    <div class={style.fullmap} >
+      <FullMap />
+    </div>
+    <div class={style.sidebar}>
+      <Sidebar />
+    </div>
+  </div>
 );
 
 export default Home;
