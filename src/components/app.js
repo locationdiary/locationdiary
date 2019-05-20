1cf12e377e033628e6724bed6cdb36afc7a1ecd0
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-05-20 21:18:43 +0800

    Add session object with blockstack logic

--- src/components/app.js
+++ src/components/app.js
@@ -1,26 +1,31 @@
-import { h, Component } from 'preact';
-import { Router } from 'preact-router';
+import { h, Component } from "preact";
+import { Router } from "preact-router";
+
+import Session from "./session";
 
 // Code-splitting is automated for routes
-import Home from '../routes/home';
+import Home from "../routes/home";
 
 export default class App extends Component {
-	
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+  /** Gets fired when the route changes.
+   *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+   *	@param {string} event.url	The newly routed URL
+   */
+  handleRoute = e => {
+    this.currentUrl = e.url;
+  };
 
-	render() {
-		return (
-			<div id="app">
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-				</Router>
-			</div>
-		);
-	}
+  componentDidMount() {
+    this.session = new Session();
+  }
+
+  render() {
+    return (
+      <div id="app">
+        <Router onChange={this.handleRoute}>
+          <Home path="/" />
+        </Router>
+      </div>
+    );
+  }
 }
---
+++ src/components/session.js
@@ -0,0 +1,30 @@
+import * as blockstack from "blockstack/dist/blockstack";
+
+const FILE_KEY = "locations.json";
+
+class Session {
+  constructor() {
+    this.userSession = new blockstack.UserSession();
+  }
+  async login() {
+    if (this.userSession.isUserSignedIn()) {
+      this.userSession.loadUserData();
+    } else if (this.userSession.isSignInPending()) {
+      await this.userSession.handlePendingSignIn();
+    } else {
+      return this.userSession.redirectToSignIn();
+    }
+  }
+  async logout() {
+    this.userSession.signUserOut();
+  }
+  async saveData(data) {
+    await this.userSession.putFile(FILE_KEY, JSON.stringify(data));
+  }
+  async getData() {
+    const fileContent = await this.userSession.getFile(FILE_KEY);
+    return JSON.parse(fileContent);
+  }
+}
+
+export default Session;
