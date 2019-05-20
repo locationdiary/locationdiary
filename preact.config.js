588143d7860b8242091e71df197650de70b7b82f
Arthur Puyou <arthur@puyou.fr>
2019-05-21 01:36:34 +0800

    Add CORS on devServer

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,12 @@
 const asyncPlugin = require("preact-cli-plugin-fast-async");
 
-export default function(config) {
+export default (config, env, helpers) => {
   asyncPlugin(config);
+  if(config.devServer) {
+    config.devServer.headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  }
 }
