c1f28f332052dae546d9e0f5598aeecb9ddeef51
Arthur Puyou <arthur@puyou.fr>
2019-12-25 22:01:01 +0100

    Upgrade Babel to fix build

--- preact.config.js
+++ preact.config.js
@@ -1,14 +1,10 @@
 export default (config, env, helpers) => {
-  if(config.devServer) {
+  if (config.devServer) {
     config.devServer.headers = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-    }
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
+    };
   }
-
-  // Load es2015 preset to transform blockstack module
-  let { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
-  let babelConfig = rule.options;
-  babelConfig.presets.push('es2015');
-}
+};
